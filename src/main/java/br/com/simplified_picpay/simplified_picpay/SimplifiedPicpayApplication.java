289759bfa4f33133f36d7902324365d2d2f4610a
vinicius.acto <vinicius.souza@acto.com.br>
2025-03-27 14:13:56 -0400

    feat: Starting the project

---
+++ src/main/java/br/com/simplified_picpay/simplified_picpay/SimplifiedPicpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.simplified_picpay.simplified_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/simplified_picpay/simplified_picpay/SimplifiedPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simplified_picpay.simplified_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
